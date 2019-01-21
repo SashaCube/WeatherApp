405785e3c95a0e13d7ef909cf214613f8b22184a
Oleksandr Havryliuk <sashagavgavgav@gmail.com>
2019-01-21 23:52:11 +0200

    add: api

---
+++ app/src/main/java/com/oleksandr/havryliuk/weatherapp/api/APIInterface.java
@@ -0,0 +1,20 @@
+package com.oleksandr.havryliuk.weatherapp.api;
+
+import com.oleksandr.havryliuk.weatherapp.Weather;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface APIInterface {
+
+    String BASE_URL = "http://api.openweathermap.org/";
+    String APP_ID = "98eaf2c433ce1765a54018269be854e8";
+
+    @GET("data/2.5/weather?q={city}&appid={APP_ID}")
+    Call<Weather> getWeaterByCity(@Path("city") String city, @Path("appid") String appId);
+
+
+}
